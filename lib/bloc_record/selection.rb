9cd0d8f58f747a4ee2b437154fe519c6560dee9e
Ben Neely <ben@bloc.io>
2015-12-02 16:57:37 -0700

    Checkpoint 03 - Add find(id1, id2) to retrieve a collection

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -1,7 +1,19 @@
 require 'sqlite3'
 
 module Selection
-  def find(id)
+  def find(*ids)
+    if ids.length == 1
+      find_one(ids.first)
+    else
+      objs = []
+      ids.each do |id|
+        objs << find_one(id)
+      end
+      objs
+    end
+  end
+
+  def find_one(id)
     row = connection.get_first_row <<-SQL
       SELECT #{columns.join ","} FROM #{table}
       WHERE id = #{id};
