f892db6174e169e8d0fdaed57b8550cb5b97a0b8
Ben Neely <ben@bloc.io>
2015-12-02 17:00:50 -0700

    Checkpoint 03 - Add take and take(num_records) to Selection module

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -22,4 +22,25 @@ module Selection
     data = Hash[columns.zip(row)]
     new(data)
   end
+
+  def take(num=1)
+    if num > 1
+      objs = []
+      num.times { objs << take_one }
+      objs
+    else
+      take_one
+    end
+  end
+
+  def take_one
+    row = connection.get_first_row <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      ORDER BY random()
+      LIMIT 1;
+    SQL
+
+    data = Hash[columns.zip(row)]
+    new(data)
+  end
 end
