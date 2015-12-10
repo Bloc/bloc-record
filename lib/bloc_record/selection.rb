7c7e2573fdb72bc0f58d095a95af1b527acb402c
Ben Neely <ben@bloc.io>
2015-12-10 14:12:38 -0800

    Checkpoint 03 - Retrieve a record by implementing .find_by(attribute, value)

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -19,7 +19,23 @@ module Selection
       WHERE id = #{id};
     SQL
 
-    data = Hash[columns.zip(row)]
-    new(data)
+    init_object_from_row(row)
+  end
+
+  def find_by(attribute, value)
+    row = connection.get_first_row <<-SQL
+      SELECT #{columns.join ","} FROM #{table}
+      WHERE #{attribute} = #{BlocRecord::Utility.sql_strings(value)};
+    SQL
+
+    init_object_from_row(row)
+  end
+
+  private
+  def init_object_from_row(row)
+    if row
+      data = Hash[columns.zip(row)]
+      new(data)
+    end
   end
 end
